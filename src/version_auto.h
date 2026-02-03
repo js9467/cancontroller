8a7481fb0eabb36192ed57ba7cc93e2272709b0b
js9467 <slaughterbeck@gmail.com>
2026-02-03 10:35:36 -0500

    Release v2.1.7

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-02-03T15:23:00.366474Z
-constexpr const char* APP_VERSION = "2.1.6";
+// Auto-generated on 2026-02-03T15:31:55.078981Z
+constexpr const char* APP_VERSION = "2.1.7";
