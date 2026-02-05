862aa78de5f65b2e1b0e67ebce0154e11cf2f15e
js9467 <slaughterbeck@gmail.com>
2026-02-04 20:19:39 -0500

    Major release v5.0.0 - Full device backup

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-02-05T00:36:10.451653Z
-constexpr const char* APP_VERSION = "4.0.3";
+// Auto-generated on 2026-02-05T01:15:40.634160Z
+constexpr const char* APP_VERSION = "5.0.0";
