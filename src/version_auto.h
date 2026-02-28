b7a46af8df7b1d533485f13dceb2c7e70269f440
js9467 <slaughterbeck@gmail.com>
2026-02-27 21:48:53 -0500

    v6.4.2 build artifacts: version header and OTA manifest

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-02-28T00:02:43.203794Z
-constexpr const char* APP_VERSION = "6.4.0";
+// Auto-generated on 2026-02-28T02:46:51.254919Z
+constexpr const char* APP_VERSION = "6.4.2";
