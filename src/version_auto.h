84c0022bdda828cd139936430881c5301c2b4cfb
js9467 <slaughterbeck@gmail.com>
2026-03-02 10:31:56 -0500

    v6.4.6: bump version for inMOTION window UP fix

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-02-28T05:18:51.525160Z
-constexpr const char* APP_VERSION = "6.4.5";
+// Auto-generated on 2026-03-02T15:28:27.405950Z
+constexpr const char* APP_VERSION = "6.4.6";
