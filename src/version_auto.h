d48c459fda168f78d2ada8b870898f138860a942
js9467 <slaughterbeck@gmail.com>
2026-03-05 13:46:51 -0500

    v6.4.8: Add live output feedback on buttons via active_output_id, active_color, active_label

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-03-05T18:13:55.580640Z
-constexpr const char* APP_VERSION = "6.4.7";
+// Auto-generated on 2026-03-05T18:46:23.524609Z
+constexpr const char* APP_VERSION = "6.4.8";
