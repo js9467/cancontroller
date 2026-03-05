e19fc92a6014d44f84b4bf9e1f85733934e33bd9
js9467 <slaughterbeck@gmail.com>
2026-03-05 14:15:48 -0500

    v6.4.9: Add IF/THEN Output Rule Engine - automated output control based on output state changes

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-03-05T18:46:23.524609Z
-constexpr const char* APP_VERSION = "6.4.8";
+// Auto-generated on 2026-03-05T19:12:49.303473Z
+constexpr const char* APP_VERSION = "6.4.9";
