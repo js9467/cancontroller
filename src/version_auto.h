e8dc4b192ae8179e14da1ca67e6727ee376ce923
js9467 <slaughterbeck@gmail.com>
2026-03-11 15:18:19 -0400

    v6.4.19: Add last_pressed feedback mode for radio-button style button groups

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-03-11T19:08:34.741663Z
-constexpr const char* APP_VERSION = "6.4.18";
+// Auto-generated on 2026-03-11T19:15:04.958643Z
+constexpr const char* APP_VERSION = "6.4.19";
