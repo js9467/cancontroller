655a3a322ba5c8de42b90df9089da4882a85b496
js9467 <slaughterbeck@gmail.com>
2026-03-15 11:21:09 -0400

    v6.4.29: Fix OTA version list: use JSON filter to handle large versions directory

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-03-15T15:16:52.557281Z
-constexpr const char* APP_VERSION = "6.4.28";
+// Auto-generated on 2026-03-15T15:18:00.271923Z
+constexpr const char* APP_VERSION = "6.4.29";
