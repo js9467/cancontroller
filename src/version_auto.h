e53f6c41e70943de796daa82f9a383f2b698b0ba
js9467 <slaughterbeck@gmail.com>
2026-03-15 00:45:49 -0400

    v6.4.24: Suspension UI: redesign to match stock TCU S15 screen with arc gauges

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-03-15T04:38:16.072846Z
-constexpr const char* APP_VERSION = "6.4.23";
+// Auto-generated on 2026-03-15T04:42:48.170401Z
+constexpr const char* APP_VERSION = "6.4.24";
