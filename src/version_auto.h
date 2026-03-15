cf6327ae8809040d45703e8b6b830e7087bf0935
js9467 <slaughterbeck@gmail.com>
2026-03-15 11:29:17 -0400

    v6.4.30: Fix suspension sync: stock unit updates our screen, our changes no longer revert

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-03-15T15:24:40.408872Z
-constexpr const char* APP_VERSION = "6.4.29";
+// Auto-generated on 2026-03-15T15:25:45.480806Z
+constexpr const char* APP_VERSION = "6.4.30";
