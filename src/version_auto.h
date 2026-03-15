f5ad1707334588caf3e2f126e1716c492aa8e60d
js9467 <slaughterbeck@gmail.com>
2026-03-15 11:06:23 -0400

    v6.4.28: Fix: changes no longer revert - 2s cooldown after TX command

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-03-15T14:54:22.238497Z
-constexpr const char* APP_VERSION = "6.4.27";
+// Auto-generated on 2026-03-15T15:03:35.525730Z
+constexpr const char* APP_VERSION = "6.4.28";
