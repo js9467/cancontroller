ec9edf6412748a4803945b2b6ce8e0c2be4c2179
js9467 <slaughterbeck@gmail.com>
2026-03-29 22:21:02 -0400

    v6.4.33: Fix scene editor crash - align suspension field IDs with HTML inputs

--- src/version_auto.h
+++ src/version_auto.h
@@ -1,3 +1,3 @@
 #pragma once
-// Auto-generated on 2026-03-15T16:46:24.009174Z
-constexpr const char* APP_VERSION = "6.4.32";
+// Auto-generated on 2026-03-29T00:00:00.000000Z
+constexpr const char* APP_VERSION = "6.4.33";
